e61ef846bd0104f3cdbf844686cabf7af88b94ee
Rhys Emmerson <rhys.emmerson@siteloft.com>
2018-02-01 13:25:39 +1000

    removed file header

--- src/Transformers/EmptyTransformer.php
+++ src/Transformers/EmptyTransformer.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * laravel-smokescreen
- *
- * User: rhys
- * Date: 1/2/18
- * Time: 11:53 AM
- */
 
 namespace RexSoftware\Laravel\Smokescreen\Transformers;
 
-
+/**
+ * Class EmptyTransformer. Used in the case where an empty resource collection is provided.
+ *
+ * @package RexSoftware\Laravel\Smokescreen\Transformers
+ */
 class EmptyTransformer extends AbstractTransformer
 {
     public function transform($data = null)
