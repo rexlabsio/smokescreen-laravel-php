25a7f5fddb29d1046ba317992488165bd46ea1a5
Andrea Marco Sartori <andrea.sartori@rexsoftware.com.au>
2018-06-15 15:42:33 +1000

    Create models finder

---
+++ src/Console/ModelsFinder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rexlabs\Laravel\Smokescreen\Console;
+
+use ReflectionClass;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * The models finder.
+ *
+ */
+class ModelsFinder
+{
+    /**
+     * Find the models in the given directory.
+     *
+     * @param string $directory
+     * @return array
+     */
+    public function findInDirectory(string $directory) : array
+    {
+        $models = [];
+        $iterator = Finder::create()->files()->name('*.php')->in($directory)->depth(0)->sortByName();
+
+        foreach ($iterator as $file) {
+            if (!class_exists($class = $this->qualifyClassFromFile($file))) {
+                continue;
+            }
+
+            $isAbstract = (new ReflectionClass($class))->isAbstract();
+            $isModel = is_subclass_of($class, 'Illuminate\Database\Eloquent\Model');
+
+            if ($isModel && !$isAbstract) {
+                $models[] = new $class;
+            }
+        }
+
+        return $models;
+    }
+
+    /**
+     * Retrieve the fully-qualified class name of the given file.
+     *
+     * @param SplFileInfo $file
+     * @return string|null
+     */
+    protected function qualifyClassFromFile(SplFileInfo $file) : ?string
+    {
+        preg_match('/namespace (.*);/', $file->getContents(), $matches);
+
+        if (is_null($namespace = $matches[1] ?? null)) {
+            return null;
+        }
+
+        return $namespace . '\\' . rtrim($file->getFilename(), '.php');
+    }
+}
